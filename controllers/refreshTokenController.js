58a5db18c7829da491dc4ccba4c1d5bb1d19cfde
Sean Leonard Chua <seanchuatech.com>
2024-06-18 14:07:51 +0800

    updated refresh token

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -3,13 +3,43 @@ const jwt = require('jsonwebtoken');
 
 const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
+    
     if (!cookies?.jwt) return res.sendStatus(401);  // Unauthorized
     const refreshToken = cookies.jwt;
 
     try {
         const foundUser = await User.findOne({ refreshToken }).exec();
-        if (!foundUser) return res.status(403).json({ error: { message: 'Forbidden (User not found)' } }); 
+        if (!foundUser) return res.status(403); // Forbidden
+        // evaluate jwt 
+        jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+            (err, decoded) => {
+                if (err || foundUser.username !== decoded.UserInfo.username) return res.sendStatus(403);
+                // if (err) {
+                //     return res.sendStatus(403);
+                // }
+                // if (foundUser.username !== decoded.username) {
+                //     console.log('db user: ', foundUser.username)
+                //     console.log('decoded user: ', decoded.username)
+                //     return res.sendStatus(403);
+                // }
+                // console.log(decoded.UserInfo.username);
+                const accessToken = jwt.sign(
+                    {
+                        "UserInfo": {
+                            "id": decoded._id,
+                            "username": decoded.username 
+                        }
+                    },
+                    process.env.ACCESS_TOKEN_SECRET,
+                    { expiresIn: '10s' }
+                );
+                res.json({ accessToken })
+            }
+        );
 
+        /* 
         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async (err, decoded) => {
             if (err || foundUser.username !== decoded.username) {
               // Revoke refresh token if there's an error
@@ -30,6 +60,8 @@ const handleRefreshToken = async (req, res) => {
             );
             res.json({ accessToken });
         });
+        */
+        
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: { message: 'Server error' } });
